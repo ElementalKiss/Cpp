a7d725fd19177f6e57cee49213da0d5d253facc4
= <=>
2015-11-27 01:45:13 +0900

    readblity?

--- Example/TernaryOperator/TernaryOperator/main.cpp
+++ Example/TernaryOperator/TernaryOperator/main.cpp
@@ -7,8 +7,11 @@ struct SomeClass
 
 SomeClass* somthingReturn() { return nullptr; }
 
+void someFunction(int num1, int num2) { return; }
+
 int main(int argc, char* argv[])
 {
+	//// simple example
 	// char pointer
 	char* str_ptr = nullptr;
 
@@ -27,6 +30,29 @@ int main(int argc, char* argv[])
 	
 	// ternary operator
 	myStruct != nullptr ? str_ptr = myStruct->str : str_ptr = "";
+
+
+	//// readblity?
+	int testNum1 = 0;
+	int testNum2 = 0;
+
+	// if - else
+	int max = 0;
+	if (testNum1 > testNum2 )
+	{
+		max = testNum1;
+	}
+	else
+	{
+		max = testNum2;
+	}
+
+	int sum = testNum1 + testNum2;
+	
+	someFunction(max, sum);
+
+	// ternary operator
+	someFunction(testNum1 > testNum2 ? testNum1 : testNum2, testNum1 + testNum2);
 
 	return 0;
 }
