682e4d9a0fcc58d4696ff9d25ac54a2278dc5f15
Elkiss-PC <jominhyuk87@gmail.com>
2015-12-29 06:19:04 +0900

    add my stack
    reviewed by me

---
+++ Source/MH_Stack/MH_Stack.h
@@ -0,0 +1,100 @@
+/*
+작성자 : 조민혁
+작성일: 2015.06.04
+작성내용
+: 스택 만들기
+
+*/
+
+#pragma once
+#include <iostream>
+#define STACKMIN -1
+
+// 스택
+template <typename T>
+class MH_Stack
+{
+private:
+	// 탁 스택
+	int m_topStack;
+	// 스택별 한계치
+	int m_maxStack;
+	// 스택 포인터
+	T* m_stackPointer;
+
+public:
+	// 생성자
+	MH_Stack(int count);
+	// 소멸자
+	~MH_Stack(void);
+
+	// 푸쉬
+	void Push(const T& pushValue);
+	// 팝
+	T Pop(void);
+	// 사이즈 얻기
+	inline int StackSize(void) { return m_topStack; } 
+	// 스택 클리어
+	void StackClear(void);
+};
+
+// 생성자
+template <typename T>
+MH_Stack<T>::MH_Stack(int stackSize)
+{
+	// 탑 스택이 -1이면 비어있는 상태
+	m_topStack = STACKMIN;
+	// 스택 최대 값을 저장한다
+	m_maxStack = stackSize;
+	// 스택 포인터 초기화
+	m_stackPointer = new T[stackSize];
+}
+
+// 소멸자
+template <typename T>
+MH_Stack<T>::~MH_Stack(void)
+{
+	// 스택 삭제
+	delete [] m_stackPointer;
+}
+
+// 푸쉬
+template <typename T>
+void MH_Stack<T>::Push(const T& tValue)
+{
+	// 스택이 가득찼다면 포인터를 탑 스택을 증가시키지 않는다.
+	if (m_topStack  == m_maxStack - 1)
+	{
+		cout<<"Stack is Full"<<endl;
+		return;
+	}
+
+	// 스택 포인터 증가시키고 값 넣기
+	m_topStack++;
+	m_stackPointer[m_topStack] = tValue;
+}
+
+// 팝
+template <typename T>
+T MH_Stack<T>::Pop(void)
+{
+	// 스택에서 뺄것이 없다면 -1(에러)리턴
+	if (m_topStack == STACKMIN)
+	{
+		cout<<"Stack is Empty"<<endl;
+		return STACKMIN;
+	}
+
+	// 리턴하고 스택포인터 감소
+	return m_stackPointer[m_topStack--];
+}
+
+// 스택 클리어
+template <typename T>
+void MH_Stack<T>::StackClear(void)
+{
+	// 파워 팝
+	while (this->m_topStack != STACKMIN) { this->Pop(); }
+	cout<<"Stack Clear."<<endl;
+	return ;
+}
---
+++ Source/MH_Stack/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include "MH_Stack.h"
+
+using namespace std;
+
+void main()
+{
+	// 캐릭터 스택 테스트
+	MH_Stack<char> charStack(10);
+	charStack.Push('A');
+	cout<<charStack.Pop()<<endl;
+
+	// 인트 스택 테스트
+	MH_Stack<int> intStack(2);
+	for (int i = 0; i < 3; i++)
+	{
+		// 스택 풀 테스트
+		intStack.Push(i);
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		// 스택 엠티 테스트
+		intStack.Pop();
+	}
+
+	for (int i = 0; i < 2; i++)
+	{
+		intStack.Push(i);
+	}
+	
+	// 현재 스택 사이즈 테스트
+	cout<<intStack.StackSize()<<endl;
+
+	intStack.StackClear();
+	 
+	// 클리어 후 빼보기
+	intStack.Pop();
+
+	// 현재 스택 사이즈 테스트
+	cout<<intStack.StackSize()<<endl;		
+
+	return;
+} 
