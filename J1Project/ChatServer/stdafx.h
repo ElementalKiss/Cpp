d5eaf888086e4222c8f837c51d66e9a27cc49e6b
Elkiss-VMWin10 <jominhyuk87@email.com>
2016-03-19 19:34:11 +0900

    Add Project. Chat Server and Client.

---
+++ J1Project/ChatServer/stdafx.h
@@ -0,0 +1,25 @@
+// stdafx.h : 자주 사용하지만 자주 변경되지는 않는
+// 표준 시스템 포함 파일 또는 프로젝트 관련 포함 파일이
+// 들어 있는 포함 파일입니다.
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#include <algorithm>
+#include <cstdlib>
+#include <deque>
+#include <iostream>
+#include <list>
+#include <set>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+#include <boost/asio.hpp>
+#include "chat_message.hpp"
+
+// TODO: 프로그램에 필요한 추가 헤더는 여기에서 참조합니다.
